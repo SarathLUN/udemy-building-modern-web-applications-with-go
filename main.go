1a680db1691e4bf7ae5bbfff05d87d2018537008
SarathLUN <lun.sarath@gmail.com>
2023-05-08 11:28:39 +0700

    add variable string & integer

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// declare a string variable
+	var whatToSay string
+	// assign a value to the variable
+	whatToSay = "Good Day World"
+	// print the value of the variable
+	println(whatToSay)
+
+	// declare a int variable
+	var i int
+	// assign a value to the variable
+	i = 7
+	// print the value of the variable
+	println(i)
+
+}
