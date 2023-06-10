d8432b49fb90ddc936a06a66d6b40961c2f6ac01
SarathLUN <lun.sarath@gmail.com>
2023-06-10 16:42:24 +0700

    finish video #24 Error checking.

--- section-03/024-Error-checking/README.md
+++ section-03/024-Error-checking/README.md
@@ -6,3 +6,7 @@
 - we also create handler function and not using closure function anymore
 - both function `Home()` and `About` is just to render respective page for responding to http client
 - we also create **constant** `portNumber` which mean it can't be changed in the running time
+- we learn how we handle error 
+- we create a handler function `Divide()` to handle request for `/divide`
+- function `Divide()` will call to another function `divideValues` that return a float and a potential error
+- if we got an error we just write it to the response, so the client web browser can view the error for this exercise.
--- section-03/024-Error-checking/main.go
+++ section-03/024-Error-checking/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-const portNumber = "localhost:8080"
+const portNumber = ":8080"
 
 // Home : handler function for "/"
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +28,30 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	fmt.Printf("starting web server on port %s", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+	http.HandleFunc("/divide", Divide)
+
+	log.Println("starting web server on port", portNumber)
+	err := http.ListenAndServe(portNumber, nil)
+	if err != nil {
+		log.Fatalln("can't start server;", err.Error())
+	}
+
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, err.Error())
+		return
+	}
+	_, _ = fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 10.0, f)
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
 }
